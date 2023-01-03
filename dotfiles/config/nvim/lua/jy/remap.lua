3d43065f87b28d1ea9ad0520217e117a09cfb817
Jacques-Yves <106453261+jacquesyvesgl@users.noreply.github.com>
2023-01-03 09:36:49 +0100

    Remap terminal

--- dotfiles/config/nvim/lua/jy/remap.lua
+++ dotfiles/config/nvim/lua/jy/remap.lua
@@ -41,5 +41,5 @@ keyset("n", "<leader>bp", vim.cmd.bp)
 keyset("n", "<leader>bd", ":bp<bar>sp<bar>bn<bar>bd<CR>")
 
 -- Terminal
-keyset({"n", "v", "i"}, "<F7>", "<ESC>:sp<CR><C-\\><C-N><C-w>j:resize 10 <bar>term<CR>")
+keyset({"n", "v", "i"}, "<leader>tt", "<ESC>:sp<CR><C-\\><C-N><C-w>j:resize 10 <bar>term<CR>")
 keyset("t", "<ESC><ESC>", "<C-\\><C-N>")
